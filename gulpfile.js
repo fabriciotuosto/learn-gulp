f53414dd7c62315b11e969bf79ff0b3c3f2254c9
Fabricio Tuosto <fabricio@scvsoft.com>
2015-04-11 20:15:36 -0300

    added connect with livereload and open index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 'user strict';
 
-var	gulp        = require('gulp');
-var	uglify      = require('gulp-uglify');
+var gulp        = require('gulp');
+var uglify      = require('gulp-uglify');
 var less        = require('gulp-less');
 var concat      = require('gulp-concat');
 var sourcemaps  = require('gulp-sourcemaps');
 var minifyCSS   = require('gulp-minify-css');
+var connect     = require('gulp-connect');
+var open        = require('gulp-open');
 var source      = require('vinyl-source-stream');
 var buffer      = require('vinyl-buffer');
-var	browserify  = require('browserify');
+var browserify  = require('browserify');
 var babelify    = require('babelify');
 var del         = require('del');
+
 
 // Build Configuration Object
 var config;
@@ -21,22 +24,23 @@ var config = require(config_file);
  * Clean generated source folders
  */
 gulp.task('clean', function(callback){
-	del([config.scripts.dest, config.styles.dest], callback);
+    del([config.scripts.dest, config.styles.dest], callback);
 });
 
 /**
  * Using Babel and EC6
  */
 gulp.task('scripts', function(){
-	return browserify({ entries: config.scripts.app, debug: true })
-		.transform(babelify)
-		.bundle()
-		.pipe(source(config.scripts.file))
-		.pipe(buffer())
-		.pipe(sourcemaps.init({loadMaps: config.sourcemaps}))
-  		.pipe(uglify({ mangle: config.scripts.mangle }))
-  		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(config.scripts.dest));
+    return browserify({ entries: config.scripts.app, debug: true })
+        .transform(babelify)
+        .bundle()
+        .pipe(source(config.scripts.file))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: config.sourcemaps}))
+        .pipe(uglify({ mangle: config.scripts.mangle }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.scripts.dest))
+        .pipe(connect.reload());;
 });
 
 /**
@@ -44,28 +48,44 @@ gulp.task('scripts', function(){
  */
 gulp.task('styles', function(){
   return gulp.src(config.styles.app)
-  			.pipe(sourcemaps.init())
-    		.pipe(less({ sourceMap : config.sourcemaps}))
-    		.pipe(minifyCSS())
-    		.pipe(sourcemaps.write('.'))
-    		.pipe(gulp.dest(config.styles.dest));
+            .pipe(sourcemaps.init())
+            .pipe(less({ sourceMap : config.sourcemaps}))
+            .pipe(minifyCSS())
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(config.styles.dest))
+            .pipe(connect.reload());
 });
 
 /**
  * Copy Bootstrap fonts to deploy directory
  */
 gulp.task('bootstrap:fonts', function(){
-	return gulp.src(config.fonts.src)  
-  				.pipe(gulp.dest(config.fonts.dest));
+    return gulp.src(config.fonts.src)  
+                .pipe(gulp.dest(config.fonts.dest));
 });
 /**
  * Using watch to recompile javascript and less
  */
 gulp.task('watch', function(){
-    gulp.watch('client/**/*.{js,less}', ['default'])
+    return gulp.watch('client/**/*.{js,less}', ['styles', 'scripts']);
 });
 
-/*
- * Running Tasks
+/**
+ *
  */
-gulp.task('default', ['clean','scripts', 'bootstrap:fonts', 'styles', 'watch'])
+ gulp.task('connect', function(){
+     connect.server({
+        root: ['public'],
+        port: 8000,
+        livereload: true
+      });
+ });
+
+gulp.task('open', function(){
+    return gulp.src('./public/index.html')
+            .pipe(open('http://localhost:8000', options));
+});
+/*
+ * Default Tasks
+ */
+gulp.task('default', ['clean','connect', 'scripts', 'bootstrap:fonts', 'styles', 'watch', 'open' ])
